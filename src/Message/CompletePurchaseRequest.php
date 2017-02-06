cc850089d3fc44a75a0c549989ebe35690fc0ec2
mayankamencherla <mayank.amencherla@razorpay.com>
2017-02-06 14:19:47 +0530

    Refactored code and removed unnecessary methods

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -4,19 +4,6 @@ namespace Omnipay\Razorpay\Message;
 
 class CompletePurchaseRequest extends PurchaseRequest
 {
-    public function getData()
-    {
-        $data = parent::getData();
-
-        return $data;
-    }
-
-    // To send the data for our
-    public function sendData($data)
-    {
-        return $this->createResponse($data);
-    }
-
     /**
      * Sending data to Response class
      */
